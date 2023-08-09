08db0254e24a3f07a3c007c5bd821a2ea92084e6
Dylan <chewliren@hotmail.com>
2023-08-09 11:33:16 +0800

    [Complete] ex00

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,31 @@
 /*   By: lchew <lchew@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 10:55:21 by lchew             #+#    #+#             */
-/*   Updated: 2023/08/09 10:55:22 by lchew            ###   ########.fr       */
+/*   Updated: 2023/08/09 11:32:13 by lchew            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int	main(int argc, char *argv[])
+{
+	int i;
+
+	i = 1;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	while (i < argc)
+	{
+		while (*argv[i])
+		{
+			std::cout << (char)toupper(*argv[i]);
+			++argv[i];
+		}
+		++i;
+	}
+	std::cout << std::endl;
+	return (0);
+}
