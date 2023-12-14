22c4493dfabe9fd784c595a44631de51e86bfe4f
Dylan Chew <lchew@student.42kl.edu.my>
2023-12-14 13:39:03 +0800

    update

--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lchew <lchew@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 20:35:48 by lchew             #+#    #+#             */
-/*   Updated: 2023/12/13 21:46:57 by lchew            ###   ########.fr       */
+/*   Updated: 2023/12/14 11:21:29 by lchew            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 int main()
 {
+	// A_Animal animal;
 	A_Animal* animal = new Dog();
 	
 	animal->makeSound();
